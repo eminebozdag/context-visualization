335d91e73bbba25f2b81179e0d46858d09c13622
eminebozdag <fyrs00@hotmail.com>
2022-11-23 16:51:39 +0100

    created Provider of first child

--- src/components/parent/page.jsx
+++ src/components/parent/page.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import MessageContext from "../../context/message.context";
 import FirstChild from "../children/child-first/page";
 import SecondChild from "../children/child-second/page";
 import "./page.css";
 const Parent = () => {
+  const [message, setMessage] = useState("Default message by Parent Node");
   return (
     <div className="container">
       <h1>Parent</h1>
-
+      <p>
+        current message: <span>{message}</span>
+      </p>
       <div className="children-container">
-        <FirstChild />
+        <MessageContext.Provider value={{ message, setMessage }}>
+          <FirstChild />
+        </MessageContext.Provider>
         <SecondChild />
       </div>
     </div>
